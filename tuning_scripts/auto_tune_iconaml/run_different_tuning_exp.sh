cd0cbfedc372a43cf9c6e2194754920e442f5aad
Arthur Grundner <b309170@levante6.lvt.dkrz.de>
2025-06-25 17:14:17 +0200

    Uploaded missing log file

--- tuning_scripts/auto_tune_iconaml/run_different_tuning_exp.sh
+++ tuning_scripts/auto_tune_iconaml/run_different_tuning_exp.sh
@@ -5,4 +5,4 @@ sbatch /work/bd1179/b309170/icon-ml_models/icon-a-ml/run/exp.auto_tune.run day o
 sbatch /work/bd1179/b309170/icon-ml_models/icon-a-ml/run/exp.auto_tune.run week refined_day # Output in log.auto_tune.12889494.o
 sbatch /work/bd1179/b309170/icon-ml_models/icon-a-ml/run/exp.auto_tune.run month refined_week # Output in log.auto_tune.12892616.o
 sbatch /work/bd1179/b309170/icon-ml_models/icon-a-ml/run/exp.auto_tune.run year refined_month # Output in log.auto_tune.12956787.o
-sbatch /work/bd1179/b309170/icon-ml_models/icon-a-ml/run/exp.auto_tune.run year refined_month_240925 # Output in log.auto_tune.12962670.o
+sbatch /work/bd1179/b309170/icon-ml_models/icon-a-ml/run/exp.auto_tune.run year refined_year # Output in log.auto_tune.12962670.o
