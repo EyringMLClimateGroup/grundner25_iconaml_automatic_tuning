f9fe460a6e4e2aadf0ee3a55e350932b33a867b9
Arthur Grundner <38557656+agrundner24@users.noreply.github.com>
2025-06-30 12:10:58 +0200

    Update run_different_tuning_exp.sh

--- tuning_scripts/auto_tune_icona_baseline/run_different_tuning_exp.sh
+++ tuning_scripts/auto_tune_icona_baseline/run_different_tuning_exp.sh
@@ -6,4 +6,4 @@ sbatch /work/bd1179/b309170/icon-ml_models/icon-a-ml/run/exp.auto_tune_baseline.
 sbatch /work/bd1179/b309170/icon-ml_models/icon-a-ml/run/exp.auto_tune_baseline.run month week_refined # Output in log.auto_tune_baseline.13850767.o
 sbatch /work/bd1179/b309170/icon-ml_models/icon-a-ml/run/exp.auto_tune_baseline.run year month_refined # Output in log.auto_tune_baseline.13852199.o
 sbatch /work/bd1179/b309170/icon-ml_models/icon-a-ml/run/exp.auto_tune_baseline.run year year_refined_1 # Output in log.auto_tune_baseline.17820065.o
-sbatch /work/bd1179/b309170/icon-ml_models/icon-a-ml/run/exp.auto_tune_baseline.run year year_refined_2 # Output in log.auto_tune_baseline.13887792.o
+sbatch /work/bd1179/b309170/icon-ml_models/icon-a-ml/run/exp.auto_tune_baseline.run year year_refined_2 # With tweak. Output in log.auto_tune_baseline.13887792.o
